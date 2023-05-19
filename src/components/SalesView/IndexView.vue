9f84b05351e4f9b37d058d23f1f069a93abc14a5
519532446@qq.com <519532446@qq.com>
2023-05-19 22:27:29 +0800

    添加并修改成v-chart组件

--- src/components/SalesView/IndexView.vue
+++ src/components/SalesView/IndexView.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>sales view</div>
+  <div>11</div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+  .echarts {
+    width: 100%;
+    height: 100%;
+  }
 </style>
